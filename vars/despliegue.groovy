dc99fda39d483003ac258c14c5898100df6b2834
Rodrigo Zuñiga <rodrigozunigab@gmail.com>
2021-01-14 10:45:31 -0300

    commit  pipeline remoto

--- vars/despliegue.groovy
+++ vars/despliegue.groovy
@@ -1,6 +1,28 @@
     def call(stageOptions, nameProject){
   
-        def ejecutarBuild = false;
+        def downloadOK = false;
+
+         stage("Validar"){
+
+           if (
+                stageOptions.contains('gitDiff')            ||
+                stageOptions.contains('nexusDownload')      ||
+                stageOptions.contains('run')                ||
+                stageOptions.contains('test')               ||
+                stageOptions.contains('gitMergeMaster')     || 
+                stageOptions.contains('gitMergeDevelop')    ||
+                stageOptions.contains('gitTagMaster')       ||
+                (stageOptions =='')
+               ) {
+               echo "Ok, se continua con los stage, ya que ingreso parametros conocidos"
+            } else {
+                currentBuild.result = 'FAILURE'
+                error ('No se puede ejecutar este pipeline, ya que no ingreso parametros conocidos')
+            }   
+
+       }
+
+
         stage("gitDiff"){   
             //Andres 
             env.TAREA =  env.STAGE_NAME  
@@ -10,15 +32,24 @@
         stage("nexusDownload"){   
             //rodrigo 
             env.TAREA =  env.STAGE_NAME  
-            echo 'stage nexusDownload'              
-            sh 'curl -X GET -u admin:Pch1axli3003 http://localhost:9000/repository/test-nexus/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar -O'
+            echo 'stage nexusDownload'   
+            if ((stageOptions.contains('nexusDownload') || (stageOptions =='')) ) {           
+                sh 'curl -X GET -u admin:admin http://localhost:8081/repository/test-nexus/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar -O' 
+                downloadOK = true;
+            } 
+
+            if (!downloadOK){
+                currentBuild.result = 'FAILURE'
+                error ('No se puede seguir ejecutando este pipeline, ya que no se descargo el artefacto')
+            }   
         } 
         stage("run"){
             //rodrigo
             env.TAREA =  env.STAGE_NAME 
-            echo 'stage run'
-            if ((stageOptions.contains('Run') || (stageOptions =='')) && (buildEjecutado) ){ 
-                sh "nohup bash gradlew bootRun &"
+            echo 'stage run'  
+
+            if ((stageOptions.contains('run') || (stageOptions =='')) && (downloadOK)){ 
+                sh "java -jar DevOpsUsach2020-0.0.1.jar &"
                 sleep 20 
             }                          
         }
@@ -26,7 +57,7 @@
             //rodrigo
             env.TAREA =  env.STAGE_NAME 
             echo 'stage test'
-            if ((stageOptions.contains('Rest') || (stageOptions =='')) && (buildEjecutado) ) 
+            if ((stageOptions.contains('test') || (stageOptions ==''))  ) 
                 sh 'curl -X GET "http://localhost:8081/rest/mscovid/test?msg=testing"'
         }  
 
