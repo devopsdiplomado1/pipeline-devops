efed4d9e3e87ea5090c7037d6ecbdd8edb8ebc31
Rodrigo Zuñiga <rodrigozunigab@gmail.com>
2021-01-14 10:28:08 -0300

    commit  pipeline remoto

---
+++ vars/gitUtils.groovy
@@ -0,0 +1,61 @@
+def crearRamaGit(String origin, String newRranch){
+    try {
+        sh '''
+            git fetch -p
+            git checkout '''+origin+'''; git pull
+            git checkout -b '''+newRranch+'''
+            git push origin '''+newRranch+'''
+        '''
+    } catch (Exception a){ } 
+
+
+}
+
+def borrarRama(String rama){
+    
+    try {
+        sh "git fetch -p"
+        sh "git branch -d  ${rama}"
+    } catch (Exception a){ }   
+
+    try {
+        sh "git push origin --delete ${rama}"
+        sh "git fetch -p"
+    } catch (Exception a){ }      
+    
+}
+
+def listarRamas(){
+
+    def getBranches = ("git ls-remote https://github.com/devopsdiplomado1/ms-iclab.git").execute()
+
+    def branchNameList = getBranches.text.readLines().collect {
+     it.split()[1]
+    }
+    def releaseBranchList = branchNameList.findAll { it =~ /refs\/heads\/release*/ }
+
+    def humanReadableReleaseBranchList = releaseBranchList.collect {
+    it.replaceAll('refs/heads/', '')
+    }
+    echo "${humanReadableReleaseBranchList}"
+    return humanReadableReleaseBranchList
+}
+
+def chequearSiExisteRama(String rama){
+    def existe = false
+    if ("${listarRamas()}".contains("${rama}"))
+        existe = true
+    /* este comando nos daba error de permisos
+    try {
+        def output = sh (script: "git ls-remote --heads ${rama}", returnStdout: true)
+        if (output?.trim()) {
+		    existe = true;
+	    } 
+    } catch (Exception a){
+        existe = false;
+
+    }*/
+
+	return existe
+
+}
--- vars/integracion.groovy
+++ vars/integracion.groovy
@@ -1,70 +1,3 @@
-def crearRamaGit(String origin, String newRranch){
-    try {
-        sh '''
-            git fetch -p
-            git checkout '''+origin+'''; git pull
-            git checkout -b '''+newRranch+'''
-            git push origin '''+newRranch+'''
-        '''
-    } catch (Exception a){ } 
-
-
-}
-
-def borrarRama(String rama){
-    
-    try {
-        sh "git fetch -p"
-        sh "git branch -d  ${rama}"
-    } catch (Exception a){ }   
-
-    try {
-        sh "git push origin --delete ${rama}"
-        sh "git fetch -p"
-    } catch (Exception a){ }      
-    
-}
-
-def listarRamas(){
-
-    def getBranches = ("git ls-remote https://github.com/devopsdiplomado1/ms-iclab.git").execute()
-
-    def branchNameList = getBranches.text.readLines().collect {
-     it.split()[1]
-    }
-    def releaseBranchList = branchNameList.findAll { it =~ /refs\/heads\/release*/ }
-
-    def humanReadableReleaseBranchList = releaseBranchList.collect {
-    it.replaceAll('refs/heads/', '')
-    }
-    echo "${humanReadableReleaseBranchList}"
-    return humanReadableReleaseBranchList
-}
-
-def chequearSiExisteRama(String rama){
-    def existe = false
-    if ("${listarRamas()}".contains("${rama}"))
-        existe = true
-    /* este comando nos daba error de permisos
-    try {
-        def output = sh (script: "git ls-remote --heads ${rama}", returnStdout: true)
-        if (output?.trim()) {
-		    existe = true;
-	    } 
-    } catch (Exception a){
-        existe = false;
-
-    }*/
-
-	return existe
-
-}
-
-
-
-
-
-
 
 def call(stageOptions, nameProject){
    def buildEjecutado = false;
@@ -145,12 +78,12 @@ def call(stageOptions, nameProject){
                 echo "STAGE ${env.STAGE_NAME}"
                 echo "entro a gitCreateRelease" 
 
-                if (chequearSiExisteRama("${nameRelease}")) {
+                if (gitUtils.chequearSiExisteRama("${nameRelease}")) {
                     echo "Se borra rama <${nameRelease}>"
-                    borrarRama("${nameRelease}")
+                    gitUtils.borrarRama("${nameRelease}")
                     echo "Se crea rama <${nameRelease}>"
-                    crearRamaGit("${env.GIT_BRANCH}", "${nameRelease}");
-                    if (chequearSiExisteRama("${nameRelease}")) {
+                    gitUtils.crearRamaGit("${env.GIT_BRANCH}", "${nameRelease}");
+                    if (gitUtils.chequearSiExisteRama("${nameRelease}")) {
                         echo "Rama <${nameRelease}> creada correctamente"
                         echo "Ahora se llama a despliegue continuo..."
                         despliegue.call(stageOptions, nameProject);
@@ -160,8 +93,8 @@ def call(stageOptions, nameProject){
                     }
                 } else {
                     echo "Se crea rama <${nameRelease}>"
-                    crearRamaGit("${env.GIT_BRANCH}", "${nameRelease}");
-                    if (chequearSiExisteRama("${nameRelease}")) {
+                    gitUtils.crearRamaGit("${env.GIT_BRANCH}", "${nameRelease}");
+                    if (gitUtils.chequearSiExisteRama("${nameRelease}")) {
                         echo "Rama <${nameRelease}> creada correctamente"
                         echo "Ahora se llama a despliegue continuo..."
                         despliegue.call(stageOptions, nameProject);
