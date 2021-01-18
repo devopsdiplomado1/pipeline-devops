ab5883499518a33c954df6d7c8a913d76b8d1463
Rodrigo Zuñiga <rodrigozunigab@gmail.com>
2021-01-18 14:54:42 -0300

    commit  pipeline remoto

--- vars/integracion.groovy
+++ vars/integracion.groovy
@@ -73,7 +73,7 @@ def call(stageOptions, nameProject){
                     timeout(1) {
                         def qg = waitForQualityGate('sonar')
 
-                        if (!qg.contains('window.serverStatus = ''UP''')) {
+                        if (!qg.contains('window.serverStatus = ''''UP''''')) {
                             Error "Fallo el llamado a Sonar, se espero 1 minuto, cambie el tiempo si es necesario o verifique la instalacion! failure: ${qg.status}"
                         }
                     }    
